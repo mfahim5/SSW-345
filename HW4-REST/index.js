0b98ef331681015ea1eb85efa62bbfd85ce0a901
mfahim5 <33581164+mfahim5@users.noreply.github.com>
2020-04-06 18:38:46 -0400

    hw 4

--- HW4-REST/index.js
+++ HW4-REST/index.js
@@ -9,6 +9,9 @@ var urlRoot = "https://api.github.com";
 var userId = "mfahim5";
 var config = {};
 // Retrieve our api token from the environment variables.
+
+
+require('dotenv').config();
 config.token = process.env.GITHUBTOKEN;
 
 if( !config.token )
@@ -26,10 +29,10 @@ if (process.env.NODE_ENV != 'test')
 {
 	(async () => {
 		await listAuthenicatedUserRepos();
-		//await listBranches(userId, "your repo");
+		await listBranches(userId, "SSW-345");
 		//await createRepo(userId,newrepo);
-		//await createIssue(userId, repo, issue);
-		//await enableWikiSupport(userId,repo);
+		await createIssue(userId, repo, issue);
+		await enableWikiSupport(userId,repo);
 
 	})()
 }
@@ -90,6 +93,7 @@ function listAuthenicatedUserRepos()
 		});
 	});
 
+
 }
 
 // 1. Write code for listBranches in a given repo under an owner. See list branches
@@ -102,20 +106,18 @@ async function listBranches(owner,repo)
 	{
 		request(options, function (error, response, body) {
 
-			// console.debug( options );
-			//resolve( JSON.parse(body) );
-			var obj = JSON.parse(body);
 
-			for( var i = 0; i < obj.length; i++ )
+
+			if( error )
 
 			{
 
-				var name = obj[i].name;
-
-				console.log( name );
+				console.log( chalk.red( error ));
+				reject(error);
+				return; // Terminate execution.
 
 			}
-
+			resolve(JSON.parse(body));
 		});
 	});
 }
@@ -123,13 +125,21 @@ async function listBranches(owner,repo)
 // 2. Write code to create a new repo
 async function createRepo(owner,repo)
 {
-	let options = getDefaultOptions("/", "POST");
+	let options = getDefaultOptions(`/user/repos`, "POST");
+	options.json=
+	{
+			name: repo
+	};
 
 	// Send a http request to url and specify a callback that will be called upon its return.
 	return new Promise(function(resolve, reject)
 	{
 		request(options, function (error, response, body) {
-
+			if(error){
+				console.log(chalk.red(error));
+				reject(error);
+				return; // Terminate execution.
+			}
 			resolve( response.statusCode );
 
 		});
@@ -139,13 +149,21 @@ async function createRepo(owner,repo)
 // 3. Write code for creating an issue for an existing repo.
 async function createIssue(owner,repo, issueName, issueBody)
 {
-	let options = getDefaultOptions("/", "POST");
+	let options = getDefaultOptions(`/repos/${owner}/${repo}/issues`, "POST");
 
+	options.json=
+	{
+		title: issueName, body: issueBody
+	}
 	// Send a http request to url and specify a callback that will be called upon its return.
 	return new Promise(function(resolve, reject)
 	{
 		request(options, function (error, response, body) {
-
+			if(error){
+				console.log(chalk.red(error));
+				reject(error);
+				return;
+			}
 			resolve( response.statusCode );
 
 		});
@@ -155,13 +173,17 @@ async function createIssue(owner,repo, issueName, issueBody)
 // 4. Write code for editing a repo to enable wiki support.
 async function enableWikiSupport(owner,repo)
 {
-	let options = getDefaultOptions("/", "PATCH");
+	let options = getDefaultOptions(`/repos/${owner}/${repo}?has_wiki=true`, "PATCH");
 
 	// Send a http request to url and specify a callback that will be called upon its return.
 	return new Promise(function(resolve, reject)
 	{
 		request(options, function (error, response, body) {
-
+			if(error){
+				console.log(chalk.red(error));
+				reject(error);
+				return;
+			}
 			resolve( JSON.parse(body) );
 		});
 	});	
